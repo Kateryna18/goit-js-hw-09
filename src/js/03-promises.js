8eb44bbf3c1c716bb60815b56e597c47cee5b85b
Kateryna18 <mikolaichukkate@icloud.com>
2023-03-13 14:03:58 +0200

    in process task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay)
+// стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу
+// (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або
+// відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position
+// і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що
+// потрібно зробити з промісом - виконати або відхилити.
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promis((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position: `${position.value}`, delay: `${delay.value}`,})
+      } else {
+        reject 
+      }
+    }, delay);
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
